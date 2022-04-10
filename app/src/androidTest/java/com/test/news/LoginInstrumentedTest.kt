a8f67baa74951b77ec08566ba7aeab8ba6d39f21
ramsilaparasetti <ram.silaparasetti@gmail.com>
2022-04-10 02:30:43 +0100

    Added first scenario tests
    Added first scenario tests

--- app/src/androidTest/java/com/test/news/LoginInstrumentedTest.kt
+++ app/src/androidTest/java/com/test/news/LoginInstrumentedTest.kt
@@ -1,36 +1,99 @@
 package com.test.news
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.clearText
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
 import com.test.news.features.login.presentation.LoginActivity
-import junit.framework.Assert.assertTrue
+import com.test.news.login.LoginRobot
 import org.junit.Rule
 import org.junit.Test
+import java.lang.Thread.sleep
 
-class LoginInstrumentedTest {
+class xLoginInstrumentedTest {
 
     @get:Rule
     var activityTestRule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
 
     @Test
-    fun shouldLoginWithValidCredentials() {
-        onView(withId(R.id.editTextUserName))
-            .perform(clearText(), typeText(VALID_USER_NAME))
-        onView(withId(R.id.editTextPassword))
-            .perform(clearText(), typeText(VALID_USER_PASSWORD))
-        onView(withId(R.id.buttonLogin))
-            .perform(click())
-
-        // TODO assert login when ready
-        assertTrue(activityTestRule.activity.isFinishing)
+    fun userOpensLoginScreen() {
+        LoginRobot()
+            .checkLoginScreen()
     }
 
-    companion object {
-        private const val VALID_USER_NAME = "user1"
-        private const val VALID_USER_PASSWORD = "password"
+    @Test
+    fun userLoginFailed() {
+        LoginRobot()
+            .enterInValidUserName()
+            .enterInValidPassWord()
+            .clickLoginButton()
+            .checkInvalidUserNameError()
+
+            .enterValidUserName()
+            .enterInValidPassWord()
+            .clickLoginButton()
+            .checkInvalidPassWordError()
     }
+
+    @Test
+    fun userLoginSucceed() {
+        LoginRobot()
+            .enterValidUserName()
+            .enterValidPassWord()
+            .clickLoginButton()
+            .checkNewsImagesDisplayed()
+    }
+
+    @Test
+    fun userOpensAppNextTime_PreviouslyLoggedIn() {
+        LoginRobot()
+            .checkLoginScreen(screenValidation = false)
+            .enterValidUserName()
+            .enterValidPassWord()
+            .clickLoginButton()
+            //.checkNewsImagesDisplayed()
+                sleep(5000)
+                        LoginRobot()
+            .pressDeviceHome()
+            .selectNewsAppFromRecents()
+            .checkNewsImagesDisplayed()
+    }
+//
+//    @Test
+//    fun loginWithInvalidUserName() {
+//
+//    }
+//
+//    @Test
+//    fun loginWithInvalidPassWord() {
+//
+//    }
+//
+//    @Test
+//    fun shouldLoginWithValidCredentials() {
+////        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("svc wifi enable")
+////        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("svc data enable")
+//        onView(withId(userNameField))
+//            .perform(clearText(), typeText(VALID_USER_NAME))
+//        onView(withId(passWordField))
+//            .perform(clearText(), typeText(VALID_USER_PASSWORD))
+//        onView(withId(loginButton))
+//            .perform(click())
+//
+//        // TODO assert login when ready
+//        assertTrue(activityTestRule.activity.isFinishing)
+//        sleep(10000)
+//        //onView(withId(R.id.recyclerViewImageWidget)).check(matches(isDisplayed()))
+//        // onView(withId(R.id.recyclerViewImageWidget)).exists()
+//        R.id.recyclerViewImageWidget.resourceMatcher().assertAny(isDisplayed())
+//
+//    }
+
+//    companion object {
+//        private const val userNameField = R.id.editTextUserName
+//        private const val passWordField = R.id.editTextPassword
+//        private const val loginButton = R.id.buttonLogin
+//        private const val VALID_USER_NAME = "user1"
+//        private const val VALID_USER_PASSWORD = "password"
+//    }
 }
+
+
+
---
+++ app/src/androidTest/java/com/test/news/login/LoginElements.kt
@@ -0,0 +1,20 @@
+package com.test.news.login
+
+import com.test.news.R
+
+object LoginElements {
+    const val newsLogo = R.id.textViewLogo
+    const val userNameField = R.id.editTextUserName
+    const val passWordField = R.id.editTextPassword
+    const val loginButton = R.id.buttonLogin
+    const val newsImageWidget = R.id.recyclerViewImageWidget
+    const val newsLogoText = "News"
+    const val VALID_USER_NAME = "user1"
+    const val VALID_USER_PASSWORD = "password"
+    const val INVALID_USER_NAME = "invaliduser"
+    const val INVALID_USER_PASSWORD = "invalidpassword"
+    const val BLANK_USER_NAME = ""
+    const val BLANK_USER_PASSWORD = ""
+    const val USER_NAME_ERROR_TEXT = "Wrong user name"
+    const val PASS_WORD_ERROR_TEXT = "Wrong password"
+}
